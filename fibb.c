a85e7a45bb8468d0906833b4f5b01d0ba6bffd9a
kusuma <kusuma.g@thundersoft.com>
2022-07-17 13:44:34 +0530

    fibbonacci series

---
+++ fibb.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int fibb( int);
+int main()
+{
+        int n,i;
+        printf("enter a number:\n");
+        scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+        printf("%d\t",fibb(i));
+	}	
+}
+int fibb(int n)
+{
+        if(n==0)
+                return 0;
+        else if(n==1)
+		return 1;
+	else
+                return (fibb(n-1)+fibb(n-2));
+}
+
+/*
+o/p:
+enter a number:
+5
+0       1       1       2       3
+*/
+
