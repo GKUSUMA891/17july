8042c489ce015a6763d200f49d3518bba9e3cf3e
kusuma <kusuma.g@thundersoft.com>
2022-07-17 13:35:10 +0530

    swap two elements using pointer

---
+++ swap-ptr.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void swap(int *,int *);
+int main()
+{
+	int a,b;
+	printf("enter a & b values:\n");
+	scanf("%d %d",&a,&b);
+        swap(&a,&b);
+}
+void swap(int *a,int *b)
+{
+	int temp=*a;
+	*a=*b;
+	*b=temp;
+	 printf("after swapping :%d %d\n ",*a,*b);
+}
+
+/*
+enter a & b values:
+10
+20
+after swapping :20 10
+*/
