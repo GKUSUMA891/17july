cb69c8c7c4f0f901a302f3d0182623df47c43d3f
kusuma <kusuma.g@thundersoft.com>
2022-07-17 17:58:15 +0530

    reverse using recursion

---
+++ reverse-recursion.c
@@ -0,0 +1,29 @@
+//reverse using recursion
+#include<stdio.h>
+void reverse(char *n)
+{
+    if(*n)
+    {
+        reverse(n+1);
+        printf("%c\n",*n);
+    }
+}
+void main()
+{
+    char m[]="morning";
+    reverse(m);
+    
+   // return 0;
+}
+/*
+o/p:
+g
+n
+i
+n
+r
+o
+m
+*/
+
+
