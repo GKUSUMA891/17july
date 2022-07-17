a63d01a094a9a956f45a55dd11542212deb8b412
kusuma <kusuma.g@thundersoft.com>
2022-07-17 14:33:56 +0530

    string reverse using recursion

---
+++ str-rev-recursion.c
@@ -0,0 +1,20 @@
+
+void reverse(char *s)
+{
+    if(*s)
+    {
+       reverse(s+1);
+       printf("%c ",*s);
+    }
+}
+
+int main()
+{
+    char s[]="thundersoft";
+    reverse(s);
+    //printf("Hello World");
+
+    return 0;
+}
+
+
