b898da6e42a262adfea5610179ceceb7f6fe652f
kusuma <kusuma.g@thundersoft.com>
2022-07-17 13:50:31 +0530

    convert decimal to binary

---
+++ dec-bin.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int convert(int);
+int main()
+{
+	int dec,bin;
+	printf("enter a decimal number:\n");
+	scanf("%d",&dec);
+	bin=convert(dec);
+	printf("binary number is :%d\n",bin);
+	return 0;
+}
+int convert(int dec)
+{ 
+	int res;
+	if(dec==0)
+		return 0;
+	else
+	{
+		res=dec%2+10*convert(dec/2);
+          return res;
+	}
+}
+
+/*
+o/p:
+enter a decimal number:
+20
+binary number is :10100
+*/
